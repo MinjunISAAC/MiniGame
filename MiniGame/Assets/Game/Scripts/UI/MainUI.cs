48c11b03a66eda8adeaaa49cb1661db74fbe002f
Min Jun <qkralswns931026@gmail.com>
2023-09-08 18:21:49 +0900

    [DEV] State View 콜 함수 구현

--- MiniGame/Assets/Game/Scripts/UI/MainUI.cs
+++ MiniGame/Assets/Game/Scripts/UI/MainUI.cs
@@ -1,6 +1,13 @@
+// ----- C#
 using System.Collections;
 using System.Collections.Generic;
+
+// ----- Unity
 using UnityEngine;
+
+// ----- User Defined
+using InGame.ForState.ForUI;
+using InGame.ForState;
 
 namespace InGame.ForUI
 {
@@ -9,14 +16,29 @@ namespace InGame.ForUI
         // --------------------------------------------------
         // Components
         // --------------------------------------------------
+        [Header("State UI")]
+        [SerializeField] private ReadyView  _readyView  = null;
+        [SerializeField] private PlayView   _playView   = null;
+        [SerializeField] private ResultView _resultView = null;
 
         // --------------------------------------------------
         // Variables
         // --------------------------------------------------
 
         // --------------------------------------------------
-        // Functions - Nomal
+        // Fucntions - Nomal
         // --------------------------------------------------
+        public StateView GetStateUI()
+        {
+            var currentState = StateMachine.Instance.CurrentState;
+            switch (currentState)
+            {
+                case EState.Ready:  return _readyView;
+                case EState.Play:   return _playView;
+                case EState.Result: return _resultView;
+                default:            return null;
+            }
+        }
 
     }
 }
