04ac195ef33ec54dd8ef04989d4b004301ea30df
Min Jun <qkralswns931026@gmail.com>
2023-09-11 12:06:53 +0900

    [DEV] Loader 구현 완료

--- MiniGame/Assets/Game/Scripts/Timer/Test.cs
+++ MiniGame/Assets/Game/Scripts/Timer/Test.cs
@@ -2,14 +2,18 @@ using InGame.ForMiniGame.ForUI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utiltiy.ForLoader;
 
 public class Test : MonoBehaviour
 {
-    [SerializeField] private TimerSystem _timer = null;
+    //[SerializeField] private TimerSystem _timer = null;
+    [SerializeField] private Loader _loader = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        _timer.Timer(TimerSystem.ECountType.CountUp, 3f, () => { Debug.Log($"End"); });
+        _loader.Visiable(3f, () => { Debug.Log($"END LOADER"); });
+        //_timer.Timer(TimerSystem.ECountType.CountUp, 3f, () => { Debug.Log($"End"); });
     }
 
     // Update is called once per frame
---
+++ MiniGame/Assets/Utility/Loader/Scripts/Loader/Loader.cs
@@ -0,0 +1,126 @@
+// ----- C#
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+namespace Utiltiy.ForLoader
+{
+    public class Loader : MonoBehaviour
+    {
+        // --------------------------------------------------
+        // Singleton
+        // --------------------------------------------------
+        // ----- Constructor
+        private Loader() { }
+
+        // ----- Static Variables
+        private static Loader _instance = null;
+
+        // ----- Variables
+        private const string FILE_PATH = "Loader.prefab";
+        private bool _isSingleton = false;
+
+        // ----- Property
+        public static Loader Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    var existingInstance = FindObjectOfType<Loader>();
+
+                    if (existingInstance != null)
+                    {
+                        _instance = existingInstance;
+                    }
+                    else
+                    {
+                        var origin = Resources.Load<Loader>(FILE_PATH);
+                        _instance = Instantiate<Loader>(origin);
+                        _instance._isSingleton = true;
+                        DontDestroyOnLoad(_instance.gameObject);
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        // --------------------------------------------------
+        // Components
+        // --------------------------------------------------
+        [SerializeField] private Animation _animation = null;
+
+        // --------------------------------------------------
+        // Variables
+        // --------------------------------------------------
+        // ----- Const
+        private const string SHOW_TRIGGER = "Loader_Show";
+        private const string IDLE_TRIGGER = "Loader_Idle";
+        private const string HIDE_TRIGGER = "Loader_Hide";
+
+        // ----- Private
+        private Coroutine _co_visiable = null;
+
+        // --------------------------------------------------
+        // Functions - Event
+        // --------------------------------------------------
+        private void Awake()
+        {
+            if (null == _instance)
+            {
+                var existingInstance = FindObjectOfType<Loader>();
+
+                if (existingInstance != null)
+                {
+                    _instance = existingInstance;
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_isSingleton)
+                _instance = null;
+        }
+
+        // --------------------------------------------------
+        // Functions - Coroutine
+        // --------------------------------------------------
+        public void Visiable(float duration, Action doneCallBack)
+        {
+            if (_co_visiable == null)
+                _co_visiable = StartCoroutine(_Co_Visiable(duration, doneCallBack));
+        }
+
+        // --------------------------------------------------
+        // Functions - Coroutine
+        // --------------------------------------------------
+        private IEnumerator _Co_Visiable(float duration, Action doneCallBack)
+        {
+            _animation.clip = _animation.GetClip(SHOW_TRIGGER);
+            _animation.Play();
+
+            var showSec = _animation.clip.length;
+            yield return new WaitForSeconds(showSec);
+
+            _animation.clip = _animation.GetClip(IDLE_TRIGGER);
+            _animation.Play();
+
+            yield return new WaitForSeconds(duration);
+
+            _animation.clip = _animation.GetClip(HIDE_TRIGGER);
+            _animation.Play();
+
+            var hideSec = _animation.clip.length;
+            yield return new WaitForSeconds(hideSec);
+
+            doneCallBack?.Invoke();
+            _co_visiable = null;
+        }
+    }
+}
